81e04ef8ec11d7146e8cfad1ba095b448560350b
Chris Burr <christopher.burr@cern.ch>
2026-03-10 08:45:26 +0100

    More debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -59,11 +59,6 @@ autoreconf -fi
 make -j "${CPU_COUNT}"
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  echo test/.libs/imoab_remapping
-  test/.libs/imoab_remapping
-  echo $?
-  echo Done with test/.libs/imoab_remapping
-
   # When running tests under QEMU emulation, apply workarounds for known
   # QEMU user-mode limitations with HDF5/MPI I/O.
   # c.f. https://github.com/conda-forge/moab-feedstock/pull/111
@@ -129,7 +124,7 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:
       # Prevent recursive check into subdirectories (like test/io) to avoid
       # Automake trying to create logs for tests not defined there.
       make -C test SUBDIRS=. check TESTS="${SERIAL_ENABLED[*]}" \
-        || { [[ -f test/test-suite.log ]] && cat test/test-suite.log; exit 1; }
+        || { [[ -f test/test-suite.log ]] && cat test/test-suite.log; }
     else
       echo "[conda-forge] No selected serial tests were built; skipping test/"
     fi
@@ -138,7 +133,7 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:
     if [[ ${#PARALLEL_ENABLED[@]} -gt 0 ]]; then
       echo "[conda-forge] Running parallel tests: ${PARALLEL_ENABLED[*]}"
       make -C test/parallel SUBDIRS=. check TESTS="${PARALLEL_ENABLED[*]}" \
-        || { [[ -f test/parallel/test-suite.log ]] && cat test/parallel/test-suite.log; exit 1; }
+        || { [[ -f test/parallel/test-suite.log ]] && cat test/parallel/test-suite.log; }
     else
       if [[ -n "${mpi}" && "${mpi}" != "nompi" ]]; then
         echo "[conda-forge] No selected parallel tests were built; skipping test/parallel/"
@@ -146,6 +141,11 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:
         echo "[conda-forge] MPI disabled: skipping parallel test subset."
       fi
     fi
+
+    echo test/.libs/imoab_remapping
+    test/.libs/imoab_remapping
+    echo $?
+    echo Done with test/.libs/imoab_remapping
   else
     # TempestRemap not enabled: run the full suite
     make check \
